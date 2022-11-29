94eff1e21c8e0af70ffabbda121c3edbb29ae2f4
raffaez <rafaeleasouzas@gmail.com>
2022-11-28 21:34:43 -0300

    feat(mobile): create button component and setup auth button

--- mobile/src/screens/SignIn.tsx
+++ mobile/src/screens/SignIn.tsx
@@ -1,10 +1,23 @@
-import { Center, Text } from 'native-base';
+import { Center, Text, Icon } from 'native-base';
+import { Fontisto } from '@expo/vector-icons';
+import Button from '../components/Button';
+
+import Logo from '../assets/logo.svg';
 
 export function SignIn(){
   return (
-    <Center flex={1} bgColor='gray.900'>
-      <Text color='white' font-Size={24} fontFamily='heading'>
-        Sign in
+    <Center flex={1} bgColor='gray.900' p={7}>
+      <Logo width={212} height={40} />
+
+      <Button
+        title="ENTRAR COM GOOGLE"
+        type="SECONDARY"
+        leftIcon={<Icon as={Fontisto} name="google" color="white" size="md"/>}
+        mt={12}
+      />
+
+      <Text color="white" textAlign="center" mt={4}>
+        Não utilizamos nenhuma informação além {'\n'} do seu e-mail para criação de sua conta.
       </Text>
     </Center>
   );
